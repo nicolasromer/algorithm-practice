fa56aff1ea6f4077b269afe0a1dd7300f208725f
nicolasromer <nicolasromer@gmail.com>
2022-09-14 11:14:37 +0200

    master: solve for diagonal

---
+++ diagonalDifference.js
@@ -0,0 +1,28 @@
+const l = require('./utils/logUtil.js');
+const testRunner = require('./utils/testRunner.js');
+
+
+const difference = (a,b) => Math.abs(a-b);
+const diagonalDifference = (matrix) => difference(...matrix.reduce((res, arr, i) => {
+	res[0] += arr[i];
+	res[1] += arr[arr.length -1 - i];
+	return res;
+}, [0,0]));
+
+const testCases = [
+    {
+        input: [ [
+            [1,2,3],
+            [2,5,5],
+            [3,4,2]
+        ]],
+        expected: Math.abs(8 - 11), 
+    },
+
+]
+
+function main() {
+    testRunner(diagonalDifference, testCases);
+}
+
+main();
